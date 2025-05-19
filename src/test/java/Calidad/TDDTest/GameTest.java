df8fcc35afebe033a577f65f0a65890f4c67cb36
RobertoAlanoca <roberto.alanoca@ucb.edu.bo>
2025-05-19 01:12:47 -0400

    Prueba de victoria diagonal Req3 prueba 4

--- src/test/java/Calidad/TDDTest/GameTest.java
+++ src/test/java/Calidad/TDDTest/GameTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GameTest {
 
 
-    @Test
+   /* @Test
     public void testNoWinnerAtStart() {// Prueba exitosa Req3 Prueba 1
         Game game = new Game();
 
@@ -39,6 +39,38 @@ public class GameTest {
         System.out.println("Ganador detectado: " + winner + "\n");
 
         assertEquals("X", winner, "X debería ganar con una línea horizontal en la fila 0");
+    }
+    @Test
+    public void testVerticalWin() { // Req3 Prueba 2
+        Game game = new Game();
+        game.placePiece(0, 0); // X
+        game.placePiece(0, 1); // O
+        game.placePiece(1, 0); // X
+        game.placePiece(1, 1); // O
+        game.placePiece(2, 0); // X -> gana
+
+        String winner = game.getWinner();
+        System.out.println("\n--- Victoria Vertical ---");
+        System.out.println("Casillas ocupadas por " + winner + ": (0,0), (1,0), (2,0)");
+        System.out.println("Ganador detectado: " + winner + "\n");
+
+        assertEquals("X", winner, "X debería ganar con una línea vertical en la columna 0");
+    }*/
+    @Test
+    public void testDiagonalWin() { // Req3 Prueba 3
+        Game game = new Game();
+        game.placePiece(0, 0); // X
+        game.placePiece(0, 1); // O
+        game.placePiece(1, 1); // X
+        game.placePiece(1, 0); // O
+        game.placePiece(2, 2); // X -> gana
+
+        String winner = game.getWinner();
+        System.out.println("\n--- Victoria Diagonal ---");
+        System.out.println("Casillas ocupadas por " + winner + ": (0,0), (1,1), (2,2)");
+        System.out.println("Ganador detectado: " + winner + "\n");
+
+        assertEquals("X", winner, "X debería ganar con una línea diagonal");
     }
 
 }
